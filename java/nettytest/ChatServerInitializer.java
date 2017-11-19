d07c1d10aa64aeeba3e81b8100a056873eac1e09
callmeqingzhi <798124928@qq.com>
2017-11-19 15:23:21 +0800

    commit ChanelPipline

---
+++ java/nettytest/ChatServerInitializer.java
@@ -0,0 +1,39 @@
+package nettytest;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * 接下来，我们需要安装我们上面实现的两个 ChannelHandler 到 ChannelPipeline。
+ * 为此，我们需要继承 ChannelInitializer 并且实现 initChannel()
+ * Created by chenz on 2017/11/19.
+ */
+public class ChatServerInitializer extends ChannelInitializer<Channel> {
+    private final ChannelGroup group;
+
+    public ChatServerInitializer(ChannelGroup group) {
+        this.group = group;
+    }
+
+    /**
+     * 添加 ChannelHandler　到 ChannelPipeline
+     * @param ch
+     * @throws Exception
+     */
+    @Override
+    protected void initChannel(Channel ch) throws Exception {            //2
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast(new HttpObjectAggregator(64 * 1024));
+        pipeline.addLast(new ChunkedWriteHandler());
+        pipeline.addLast(new HttpRequestHandler("/ws"));
+        pipeline.addLast(new WebSocketServerProtocolHandler("/ws"));
+        pipeline.addLast(new TextWebSocketFrameHandler(group));
+    }
+}
